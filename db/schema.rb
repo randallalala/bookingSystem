70f5ce297693ce5b483af965f46854e9939f0c71
randall <randall.lim.lim.lim@hotmail.com>
2021-10-12 01:39:30 +0800

    trainer and client scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_11_173040) do
+ActiveRecord::Schema.define(version: 2021_10_11_173858) do
+
+  create_table "clients", force: :cascade do |t|
+    t.string "photo"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone"
+    t.text "bio"
+    t.integer "user_id"
+    t.integer "account_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["account_id"], name: "index_clients_on_account_id"
+    t.index ["user_id"], name: "index_clients_on_user_id"
+  end
 
   create_table "lessons", force: :cascade do |t|
     t.string "image"
@@ -23,6 +37,21 @@ ActiveRecord::Schema.define(version: 2021_10_11_173040) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "trainers", force: :cascade do |t|
+    t.string "photo"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone"
+    t.text "bio"
+    t.string "experience"
+    t.integer "user_id"
+    t.integer "account_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["account_id"], name: "index_trainers_on_account_id"
+    t.index ["user_id"], name: "index_trainers_on_user_id"
   end
 
 end
